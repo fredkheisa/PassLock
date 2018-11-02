61f34692bd0017d2f836b4033342f8fc60eacaab
Github User Name <you@example.com>
2018-11-02 12:11:21 +0300

    ctrating test for the whole application

---
+++ application_tests.py
@@ -0,0 +1,21 @@
+import unittest
+from accclass import Account
+from infoclass import Info
+
+class TestAccount(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the Account class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+class TestInfo(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the Info class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
