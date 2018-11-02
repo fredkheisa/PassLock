2a775be47e3b73a19d46458e85c98197bda5ef3a
Github User Name <you@example.com>
2018-11-02 14:50:44 +0300

    fist test on account setup ran successfully

--- accclass.py
+++ accclass.py
@@ -1,14 +1,15 @@
-class Account : 
-    """
-    Class that generates instances on the accounts 
-    
-    """
+class Account:
+  """
+  Class that generates new instances of contacts.
+  """
 
-    def __init__(self,first_name,last_name,phone_number,email):
+  account_list = [] # Empty contact list
 
-        # docstring removed for simplicity
+  def __init__(self,first_name,last_name,phone_number,email):
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone_number = number
-        self.email = email
+    # docstring removed for simplicity
+      
+    self.first_name = first_name
+    self.last_name = last_name
+    self.phone_number = phone_number
+    self.email = email
--- application_tests.py
+++ application_tests.py
@@ -1,6 +1,5 @@
-import unittest
-from accclass import Account
-from infoclass import Info
+import unittest # Importing the unittest module
+from accclass import Account # Importing the contact class
 
 class TestAccount(unittest.TestCase):
 
@@ -10,12 +9,23 @@ class TestAccount(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_accclass = Account("Fred","Wamalwa","0702049058","fredkheisa@gmail.com") # create contact object
 
-class TestInfo(unittest.TestCase):
 
-    '''
-    Test class that defines test cases for the Info class behaviours.
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
 
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-    '''
+        self.assertEqual(self.new_accclass.first_name,"Fred")
+        self.assertEqual(self.new_accclass.last_name,"Wamalwa")
+        self.assertEqual(self.new_accclass.phone_number,"0702049058")
+        self.assertEqual(self.new_accclass.email,"fredkheisa@gmail.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
