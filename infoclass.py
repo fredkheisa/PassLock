0f4309f433bfe64873a58f29104e198689b6d663
Github User Name <you@example.com>
2018-11-02 12:00:45 +0300

    created the file for the application tests

---
+++ infoclass.py
@@ -0,0 +1,14 @@
+class Info :
+    """
+    Class that generates instances on the information inside the accounts
+
+    """
+
+    def __init__(self,account_name,password):
+
+        # docstring removed for simplicity
+
+        self.account_name = account_name
+        self.password = password
+
+
